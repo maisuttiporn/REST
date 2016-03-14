cb5151b60622421785e3ae2fc2035ea16ca5abfc
maisuttiporn <maisuttiporn@gmail.com>
2016-03-14 17:49:29 +0700

    save

--- index.php
+++ index.php
@@ -4,11 +4,8 @@ header("Content-Type:application/json");
 if(!empty($_GET["name"])) {
     //
     $name = $_GET["name"];
-    $price = get_price($name);
-    
-    
-    
-    
+    $price = get_price($name);    
+  
     if(empty($price)) {
         // book not found
         deliver_response(200,"book not found",NULL);
